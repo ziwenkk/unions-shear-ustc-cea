51265a664b25d1eadbb83f28f82ffcfdea8ee301
Martin Kilbinger <martin.kilbinger@cea.fr>
2022-09-09 10:46:40 +0200

    Added (first version, adapted from Lisa) of z_to_dndz to transform Hendrik's blinded z samples to dndz

--- scripts/z_to_dndz.py
+++ scripts/z_to_dndz.py
@@ -4,26 +4,48 @@ import matplotlib.pylab as plt
 
 work_dir = '.'
 
-cat_name = work_dir + '/lensfit_goldshape_2022v1.fits'
-nz_hdu1 = work_dir + '/blind_nz_cfis_lensfit_goldshape_2022v1.fits'
-
-hdu = fits.open(cat_name)
-hdu.info()
-data = hdu1[1].data
+nz_names = [
+    'blind_nz_cfis_shapepipe_2022v1.fits',
+    'blind_nz_cfis_lensfit_goldshape_2022v1.fits'
+]
+methods = ['SP', 'LF']
 
 blinds = ['A','B','C']
 
+for nz_name, method in zip(nz_names, methods):
+    for blind in blinds:
 
-for blind in blinds:
-    hdu = fits.open(nz_hdu1)
-    z1 = hdu[1].data['Z_%s' %blind]
+        # Open FITS file with (SOM-derived) redshifts
+        hdu = fits.open(f'{work_dir}/{nz_name}')
+        z1 = hdu[1].data['Z_%s' %blind]
 
-    (n,bins,_)= plt.hist(z1, bins=200, range=(0,5.0), density=True, histtype='step', weights=None,label='LensFit blind_%s' %blind)
+        # Compute and plot histogram
+        n, bins, _ = plt.hist(
+            z1,
+            bins=200,
+            range=(0, 5.0),
+            density=True,
+            histtype='step',
+            label=f'{method} blind_{blind}'
+        )
 
-    plt.xlabel('Redshifts')
-    plt.ylabel('n(z)')
-    print("zmin = ",min(z1))
-    print("zmax = ",max(z1))
-    plt.legend(fontsize=20)
+        plt.xlabel('Redshifts')
+        plt.ylabel('n(z)')
+        plt.legend(fontsize=20)
 
-    plt.show()
+        # Save plot
+        output_base = f'dndz_{method}_{blind}'
+        plt.savefig(f'{output_base}.png')
+
+        print(f'{method} {blind} {min(z1)} {max(z1)}')
+
+        # Add zero to right-most bin boundary
+        n = np.append(n, 0.0)
+
+        # Save histogram as ASCII file
+        np.savetxt(
+            f'{output_base}.txt',
+            np.column_stack((bins, n)),
+            header='z dn_dz',
+        )
+
